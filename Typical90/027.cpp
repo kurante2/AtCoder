725bfd835202c74f58368bffad06a0c2cb62f5c8
kurante <sougetsumichiyasu@gmail.com>
2023-06-16 13:06:45 +0900

    AC, setを用いたが想定解はmap

---
+++ Typical90/027.cpp
@@ -0,0 +1,37 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <numeric> // gcd
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    set<string> S;
+    string tmp;
+    
+    for(int i = 1; i <= N; i++) {
+        cin >> tmp;
+        if(S.count(tmp)) continue;
+        else {
+            cout << i << endl;
+            S.insert(tmp);
+        }
+    }
+}
